d791dbe5b97fecb0f284ebf277e59074491d1686
unknown <kofigo375@gmail.com>
2024-02-28 21:58:48 +0000

    add files

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 ## this module will help us use python classes to write html forms 
 
 from flaskblog.models import User
+from flask_login import current_user
 
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -40,4 +41,29 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
+    
+## creating update account form
+class UpdateAccountForm(FlaskForm): 
+    ## this class inherits from the main class FlaskForm
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=30)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    
+    submit = SubmitField('Update')
+    
+    def validate_username(self, username):
+        if username.data != current_user.username: 
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already exist. Please choose a different username')
+            
+    def validate_email(self, email): 
+        if email.data != current_user.email: 
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('email already exist. Please choose a different email')
+                
+
     
